11cb213577bd89d3e5e64abddcb908435a87cfd3
Andrey Burikhin <Andrey_Burikhin@epam.com>
2017-06-05 16:54:40 +0400

    [BACKLOG-16405] - Parquet Output step prototype

--- kettle-plugins/parquet/src/main/java/org/pentaho/big/data/kettle/plugins/parquet/output/ParquetOutputMeta.java
+++ kettle-plugins/parquet/src/main/java/org/pentaho/big/data/kettle/plugins/parquet/output/ParquetOutputMeta.java
@@ -1,6 +1,16 @@
 package org.pentaho.big.data.kettle.plugins.parquet.output;
 
+import java.util.Collections;
+import java.util.List;
+
+import org.pentaho.di.core.Const;
 import org.pentaho.di.core.annotations.Step;
+import org.pentaho.di.core.database.DatabaseMeta;
+import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.di.core.exception.KettleXMLException;
+import org.pentaho.di.core.xml.XMLHandler;
+import org.pentaho.di.repository.ObjectId;
+import org.pentaho.di.repository.Repository;
 import org.pentaho.di.trans.Trans;
 import org.pentaho.di.trans.TransMeta;
 import org.pentaho.di.trans.step.BaseStepMeta;
@@ -8,12 +18,18 @@ import org.pentaho.di.trans.step.StepDataInterface;
 import org.pentaho.di.trans.step.StepInterface;
 import org.pentaho.di.trans.step.StepMeta;
 import org.pentaho.di.trans.step.StepMetaInterface;
+import org.pentaho.metastore.api.IMetaStore;
+import org.w3c.dom.Node;
 
 @Step( id = "ParquetOutput", image = "HBO.svg", name = "HBaseOutput.Name", description = "HBaseOutput.Description",
     categoryDescription = "i18n:org.pentaho.di.trans.step:BaseStep.Category.BigData",
-    documentationUrl = "http://wiki.pentaho.com/display/EAI/HBase+Output",
-    i18nPackageName = "org.pentaho.di.trans.steps.parquet" )
+    documentationUrl = "http://wiki.pentaho.com/display/EAI/Parquet+output",
+    i18nPackageName = "org.pentaho.di.trans.steps.parquet", isSeparateClassLoaderNeeded = true )
 public class ParquetOutputMeta extends BaseStepMeta implements StepMetaInterface {
+
+  private String filename;
+
+  private List<ParquetOutputField> outputFields = Collections.emptyList();
 
   @Override
   public void setDefault() {
@@ -29,6 +45,102 @@ public class ParquetOutputMeta extends BaseStepMeta implements StepMetaInterface
   @Override
   public StepDataInterface getStepData() {
     return new ParquetOutputData();
+  }
+
+  public String getFilename() {
+    return filename;
+  }
+
+  public void setFilename( String filename ) {
+    this.filename = filename;
+  }
+
+  public List<ParquetOutputField> getOutputFields() {
+    return outputFields;
+  }
+
+  public void setOutputFields( List<ParquetOutputField> outputFields ) {
+    this.outputFields = outputFields;
+  }
+
+  @Override
+  public void loadXML( Node stepnode, List<DatabaseMeta> databases, IMetaStore metaStore ) throws KettleXMLException {
+    readData( stepnode, metaStore );
+  }
+
+  private void readData( Node stepnode, IMetaStore metastore ) throws KettleXMLException {
+    try {
+      filename = XMLHandler.getTagValue( stepnode, "filename" );
+      Node fields = XMLHandler.getSubNode( stepnode, "fields" );
+      for ( int i = 0; i < outputFields.size(); i++ ) {
+        Node fnode = XMLHandler.getSubNodeByNr( fields, "field", i );
+        ParquetOutputField outputField = new ParquetOutputField();
+        outputField.setName( XMLHandler.getTagValue( fnode, "name" ) );
+        outputField.setType( XMLHandler.getTagValue( fnode, "type" ) );
+      }
+    } catch ( Exception e ) {
+      throw new KettleXMLException( "Unable to load step info from XML", e );
+    }
+  }
+
+  @Override
+  public String getXML() {
+    StringBuffer retval = new StringBuffer( 800 );
+
+    retval.append( "    " + XMLHandler.addTagValue( "filename", filename ) );
+
+    retval.append( "    <fields>" ).append( Const.CR );
+    for ( int i = 0; i < outputFields.size(); i++ ) {
+      ParquetOutputField field = outputFields.get( i );
+
+      if ( field.getName() != null && field.getName().length() != 0 ) {
+        retval.append( "      <field>" ).append( Const.CR );
+        retval.append( "        " ).append( XMLHandler.addTagValue( "name", field.getName() ) );
+        retval.append( "        " ).append( XMLHandler.addTagValue( "type", field.getTypeDesc() ) );
+        retval.append( "      </field>" ).append( Const.CR );
+      }
+    }
+    retval.append( "    </fields>" ).append( Const.CR );
+
+    return retval.toString();
+  }
+
+  @Override
+  public void readRep( Repository rep, IMetaStore metaStore, ObjectId id_step, List<DatabaseMeta> databases )
+    throws KettleException {
+    try {
+      filename = rep.getStepAttributeString( id_step, "filename" );
+
+      int nrfields = rep.countNrStepAttributes( id_step, "field" );
+
+      for ( int i = 0; i < nrfields; i++ ) {
+        ParquetOutputField outputField = new ParquetOutputField();
+
+        outputField.setName( rep.getStepAttributeString( id_step, i, "name" ) );
+        outputField.setType( rep.getStepAttributeString( id_step, i, "type" ) );
+
+        outputFields.add( outputField );
+      }
+
+    } catch ( Exception e ) {
+      throw new KettleException( "Unexpected error reading step information from the repository", e );
+    }
+  }
+
+  @Override
+  public void saveRep( Repository rep, IMetaStore metaStore, ObjectId id_transformation, ObjectId id_step )
+    throws KettleException {
+    try {
+      rep.saveStepAttribute( id_transformation, id_step, "filename", filename );
+      for ( int i = 0; i < outputFields.size(); i++ ) {
+        ParquetOutputField field = outputFields.get( i );
+
+        rep.saveStepAttribute( id_transformation, id_step, i, "name", field.getName() );
+        rep.saveStepAttribute( id_transformation, id_step, i, "type", field.getTypeDesc() );
+      }
+    } catch ( Exception e ) {
+      throw new KettleException( "Unable to save step information to the repository for id_step=" + id_step, e );
+    }
   }
 
 }
